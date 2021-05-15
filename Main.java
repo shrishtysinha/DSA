61212598fd25d354a7b2d01a5a8eeec5d6d65c96
shrishty <shrishty0618@gmail.com>
2021-05-15 23:34:07 +0530

    Added Java implementation for Bubble Sort

--- Main.java
+++ Main.java
@@ -1,42 +1,84 @@
 package com.company;
-import java.util.Scanner;
 
-public class Main {
+//Bubble sort without flag, t(n) = o(n^2)
+/*public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        int arr[] = new int[5];
-        System.out.println("Enter the unsorted array");
-        Scanner sc = new Scanner(System.in);
-        for(int i = 0; i< arr.length;i++){
-            arr[i] = sc.nextInt();
-        }
-        System.out.println("Before Insertion sort");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ",");
+        int i , j;
+        int arr [] = { 1,7,-1,2,3,8};
+        System.out.println("Before Sorting");
+        for(i=0;i<arr.length-1;i++){
+            System.out.print(arr[i]+",");
+
 
         }
-
         System.out.println("");
-        for (int j = 1; j < arr.length; j++) {
-            int key = arr[j];
-            int i = j - 1;
-            while (i >= 0 && arr[i] > key) {
-                arr[i + 1] = arr[i];
-                i = i - 1;
+        for(i=0;i<arr.length-1;i++) { // Each Pass runs n-1 times
+            for(j=0;j<arr.length-(i+1);j++) { // After every pass this will reduce 'i+1' times since i starts from 0
+                if(arr[j] > arr [j+1]){ // stable algorithm > not >=
+                    swap(arr , j , j+1);
+                }
             }
-            arr[i+ 1] = key;
         }
-        System.out.println("After Insertion sort");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ",");
+        System.out.println("After sorting");
+        for(i=0;i<arr.length-1;i++){
+            System.out.print(arr[i]+ ",");
 
         }
+        System.out.println("");
+
+
     }
+    public static void swap(int arr[], int i , int j)
+    {
+        int temp;
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }*/
+
+    //Bubble sort with flag, t(n) = o(n^2)
+    // If array is presorted or adaptable then reduce then don't wasting running through all pass
+    // T(n) = o(n)
+    public class Main {
+
+        public static void main(String[] args) {
+            int i , j;
+            int flag = 0;
+            int arr [] = { 1,7,-1,2,3,8};
+            System.out.println("Before Sorting");
+            for(i=0;i<arr.length-1;i++){
+                System.out.print(arr[i]+",");
+
+
+            }
+            System.out.println("");
+            for(i=0;i<arr.length-1;i++) { // Each Pass runs n-1 times
+                for(j=0;j<arr.length-(i+1);j++) { // After every pass this will reduce 'i+1' times since i starts from 0
+                    if(arr[j] > arr [j+1]){
+                        swap(arr , j , j+1);
+                        flag = 1;
+                    }
+                }
+                if(flag == 0)
+                    break;
+            }
+            System.out.println("After sorting");
+            for(i=0;i<arr.length-1;i++){
+                System.out.print(arr[i]+ ",");
+
+            }
+            System.out.println("");
+
+
+        }
+        public static void swap(int arr[], int i , int j)
+        {
+            int temp;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
 }
-// No swaps only shifts
-// Best algorithm if input size is small
-// stable algorithm due to "arr[i] > key"
-// Best case t(n) = o(n), when input is sorted in increasing order, no shifts only comparison
-// worst and average case t(n) = o(n^2)
-// Adaptable and Inplace algo.
+ //Bubble sort : Stable, adaptable , Inplace
